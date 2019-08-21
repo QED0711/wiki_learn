049f881bb471f686879a56caa2aa3cea532adbe1
QED0711 <qdizon@gmail.com>
2019-08-21 04:54:25 -0400

    can get plain title from scrapped intro links

--- utils/url_utils.py
+++ utils/url_utils.py
@@ -3,5 +3,9 @@ from urllib.parse import unquote
 def parse_url(url):
     return unquote(url)
 
+def get_title(url):
+    return parse_url(url.split("/")[-1]).replace("_", " ")
+
+
 if __name__ == "__main__":
     print(parse_url("https://en.wikipedia.org/wiki/Elisabeth_R%C3%B6ckel"))
