d7b90cb9895aad8fd63481f45547b9dc0be435c0
Quinn Dizon <qdizon@gmail.com>
2019-08-21 20:39:01 -0400

    incorperated wiki_multi_query into graph_api

---
+++ utils/WikiMultiQuery.py
@@ -0,0 +1,165 @@
+import requests
+
+
+# Helper Functions
+
+def query_wiki_api(params):
+    url="https://en.wikipedia.org/w/api.php"
+    return requests.get(url, params).json()
+
+def get_pages(resp):
+    return list(resp['query']["pages"].keys())
+
+def page_dicts(resp):
+    pages = get_pages(resp)
+    return [{"pageid": page} for page in pages]
+
+def add_title(resp, pages):
+    for article in pages:
+        try:
+            pageid = article['pageid']
+            article['title'] = resp['query']['pages'][pageid]['title']
+        except:
+            continue
+    return pages
+
+def add_links(resp, pages):
+    for article in pages:
+        try:
+            pageid = article['pageid']
+            links = [link['title'] for link in resp['query']['pages'][pageid]['links']]
+            if "links" in article:
+                article['links'] += links
+            else:
+                article['links'] = links
+        except:
+            continue
+            
+    return pages
+
+def add_linkshere(resp, pages):
+    for article in pages:
+        try:
+            pageid = article['pageid']
+            links = [link['title'] for link in resp['query']['pages'][pageid]['linkshere']]
+            if "linkshere" in article:
+                article['linkshere'] += links
+            else:
+                article['linkshere'] = links
+        except:
+            continue
+            
+    return pages
+
+def add_categories(resp, pages):
+    for article in pages:
+        try:
+            pageid = article['pageid']
+            categories = [cat['title'] for cat in resp['query']['pages'][pageid]['categories']]
+            if "categories" in article:
+                article['categories'] += categories
+            else:
+                article['categories'] = categories
+        except:
+            continue
+            
+    return pages
+
+def update_continue(resp, params):
+    if resp.get("continue"):
+        # remove any previous continue strings
+        keys = list(params.keys())
+        for key in keys:
+            if "continue" in key:
+                del params[key]
+                
+        # update with new params
+        params.update(resp.get("continue"))        
+        return params
+    else:
+        return False
+
+######################
+## Wiki Multi Query ##
+######################
+
+def wiki_multi_query(articles, params=None, pages=None):
+    """
+    Accepts multiple article titles and returns link, linkhere, and category information for each.
+
+    Input:
+    ------
+    articles (required) 
+    A list of article titles
+
+    params (optional)
+    A dictionary of parameters. Must include at least, {"action": "query", "format": "json"}. 
+    If no params are set, they will be automatically set.
+
+    pages (None)
+    This value will be set automatically upon recursive calls as necessary. 
+
+    Returns:
+    --------
+    A list of dictionaries containing each article's information.
+    """
+
+    if not params:
+        params = {
+            "action": "query",
+            "format": "json",
+
+            "prop": "redirects|links|linkshere|categories",
+
+            # redirects
+            "rdnamespace": 0,
+            "rdlimit": "max",
+
+            # links
+            "pllimit": "max",
+            "plnamespace": 0,
+
+            # linkshere
+            "lhlimit": "max",
+            "lhnamespace": 0,
+            "lhshow": "!redirect",
+
+            # categories
+            "cllimit": "max",
+
+            # automatic redirect
+            "redirects": 1
+        }
+
+
+
+    params['titles'] = "|".join(articles)
+
+    resp = query_wiki_api(params)
+
+    if not pages:
+        pages = page_dicts(resp)
+
+    pages = add_title(resp, pages)
+    pages = add_links(resp, pages)
+    pages = add_linkshere(resp, pages)
+    pages = add_categories(resp, pages)
+
+    # will return an updated params with continue statements OR False
+    params = update_continue(resp, params)
+
+    # if params still is truthy, then it was updated with a continue
+    # start the process again on the continued work
+    if params:
+        return wiki_multi_query(articles, params, pages)
+
+    return pages
+
+
+
+if __name__ == "__main__":
+    wiki_multi_query(["Random forest", "Decision tree", "Machine learning", "Sonata"])[3]['linkshere']
+
+
+
+
