75ff0a69e862a446962812d55c1c3ad92c04139f
QED0711 <qdizon@gmail.com>
2019-08-22 10:14:02 -0400

    added sort_dict_values function to graph_helpers

--- utils/graph_helpers.py
+++ utils/graph_helpers.py
@@ -2,8 +2,14 @@ import pdb
 import pandas as pd
 import networkx as nx
 
+def sort_dict_values(dict, columns, sort_column, ascending=False):
+    to_list = [(key, value) for key, value in dict.items()]
+    return pd.DataFrame(to_list, columns=columns).sort_values(sort_column, ascending=ascending).reset_index().drop("index", axis=1)
+
 def create_dispersion_df(G, central_node, node_list):
     dispersion = [(central_node, node, nx.dispersion(G, central_node, node)) for node in node_list]
     return pd.DataFrame(dispersion, columns=["entry", "node", "dispersion"])
+
+
 
 
