10084951145aee631f9441aaaa865f2069cb3878
Quinn Dizon <qdizon@gmail.com>
2019-08-22 20:00:01 -0400

    updated dispersion method

--- utils/graph_helpers.py
+++ utils/graph_helpers.py
@@ -7,6 +7,7 @@ def sort_dict_values(dict, columns, sort_column, ascending=False):
     return pd.DataFrame(to_list, columns=columns).sort_values(sort_column, ascending=ascending).reset_index().drop("index", axis=1)
 
 def create_dispersion_df(G, central_node, node_list):
+    print("MAKING DISPERSION")
     dispersion = [(central_node, node, nx.dispersion(G, central_node, node)) for node in node_list]
     return pd.DataFrame(dispersion, columns=["entry", "node", "dispersion"]).sort_values("dispersion", ascending=False).reset_index().drop("index", axis=1)
 
