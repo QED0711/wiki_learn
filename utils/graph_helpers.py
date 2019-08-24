e059ed189310c6fd18f061f2498c2007f24f4013
Quinn Dizon <qdizon@gmail.com>
2019-08-23 20:57:02 -0400

    added edge weights from category matches

--- utils/graph_helpers.py
+++ utils/graph_helpers.py
@@ -11,6 +11,18 @@ def create_dispersion_df(G, central_node, node_list):
     dispersion = [(central_node, node, nx.dispersion(G, central_node, node)) for node in node_list]
     return pd.DataFrame(dispersion, columns=["entry", "node", "dispersion"]).sort_values("dispersion", ascending=False).reset_index().drop("index", axis=1)
 
+def format_categories(cat_list):
+    cat_dict = {}
+    for cat in cat_list:
+        cat_dict[cat] = True    
+    return cat_dict
 
-
-
+def compare_categories(node1, node2, categories):
+    match_count = 1
+    try:
+        for cat in categories[node1].keys():
+            if categories[node2].get(cat):
+                match_count += 1
+        return match_count
+    except:
+        return 1
